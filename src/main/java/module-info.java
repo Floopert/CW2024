926a38030759556ce0d8c64e5282616170623960
Floopert <ngwengjie@hotmail.com>
2024-11-13 22:32:04 +0800

    -Fixes a warning that states that javafx.graphics is not transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
 
+    //might need this if using JavaFX properties
+    //requires javafx.base;
 
+    //only need to include new packages if that package uses fxml controllers (fxml references objects in the package)
     opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
 }
